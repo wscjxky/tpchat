2c5e28d730f7b70b7a8e958ad6519e9430810f4c
619677632@qq.com <619677632@qq.com>
2018-08-08 14:21:22 +0800

    init

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,11 +4,11 @@ return array(
 	'URL_CASE_INSENSITIVE'  =>  true,
 	//数据库配置
 	'DB_TYPE'=>'mysql',
-	'DB_HOST'=>'182.92.97.97',
+	'DB_HOST'=>'39.107.82.164',
 	'DB_NAME'=>'tpchat',//think_yit
     'DB_PORT'   => 3306,
 	'DB_USER'=>'root',
     'DB_CHARSET'=> 'utf8',
-	'DB_PWD'=>'HuiTeng168',
+	'DB_PWD'=>'10@idccom',
 
 );
